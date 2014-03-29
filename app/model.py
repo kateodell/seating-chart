399aa85bdd34243c8d03b43442ab58348662f032
Gowri <gowri@gowri-VBox.(none)>
2014-03-29 14:58:41 -0700

    Python Model

---
+++ app/model.py
@@ -0,0 +1,47 @@
+#hackbright team at Facebook FemEngineers Hackathon
+#Gowri & Kate
+
+#Assignment definition: Model for WEdding tables app
+
+import sqlite3
+#import traceback
+
+DB = None
+CONN = None
+
+class Attendee(object):
+    def __init__(self, id, first_name, last_name, photo, hometown):
+        self.id = id
+        self.first_name = first_name
+        self.last_name = last_name
+        self.photo = photo  #URL
+        self.hometown = hometown
+
+
+    def add_new_attendees(self):
+        # call FB API for the given event and pull down any new attendees
+
+        return record
+
+
+class Table(object):
+    def __init__(self, id, seating_list):
+        self.id = id
+        self.seating_list= seating_list # list of Attendees
+
+    def add_users_to_table(self):
+        sql = """INSERT into Tables (id, seating_list) VALUES (?, ?, ?, ?)"""
+        DB.execute(sql, (self.id, self.seating_list))
+        CONN.commit()
+
+def connect_to_db():
+    global DB, CONN
+    CONN = sqlite3.connect("hackbright.db")
+    DB = CONN.cursor()
+
+def get_all_tables():
+    sql = """SELECT * FROM Tables"""
+    DB.execute(sql)
+    records = DB.fetchall()
+
+    return records
