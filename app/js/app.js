09a9722822a8054aa45f5e85e6841a2bc94248a6
Kate Odell <kate.m.odell@gmail.com>
2014-03-29 17:03:37 -0700

    Makes requests to get event attendees and their info to display

--- app/js/app.js
+++ app/js/app.js
@@ -3,22 +3,20 @@
 
 // Declare app level module which depends on filters, and services
 angular.module('myApp', [
+  'ezfb',
   'ngRoute',
   'myApp.filters',
   'myApp.services',
   'myApp.directives',
-  'myApp.controllers',
-  'ezfb'
-]).
-
+  'myApp.controllers'
+  ]).
 config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
   $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
   $routeProvider.otherwise({redirectTo: '/view1'});
-}]);
-
-.config(function (ezfbProvider) {
+}]).
+config(function(ezfbProvider) { // provider-injector
   ezfbProvider.setInitParams({
     appId: '1419342494987425'
-  });  
+  });
 });
--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,9 +3,43 @@
 /* Controllers */
 
 angular.module('myApp.controllers', []).
-  controller('MyCtrl1', [function() {
-    $scope.guest_list = ['Kate', 'Gowri', 'Arjan', 'Ravi', 'Dusty'];
-  }])
+  controller('MyCtrl1', function ($scope, ezfb) {
+    // $scope.guest_list = ['Kate', 'Gowri', 'Arjan', 'Ravi', 'Dusty'];
+    ezfb.login();
+    ezfb.getLoginStatus(function (res) {
+      $scope.loginStatus = res;
+      console.log($scope.loginStatus);
+      $scope.getEventInfo();
+    });
+
+
+    $scope.getEventInfo = function(){
+      ezfb.api('https://graph.facebook.com/630852236995897/', function (res) {
+        $scope.eventInfo = res;
+        console.log(res)
+      });
+
+      ezfb.api('https://graph.facebook.com/630852236995897/attending', function(res){
+        $scope.attendees = res.data;
+        $scope.getAttendeeInfo();
+      });
+
+      
+    };
+
+    $scope.getAttendeeInfo = function(){
+      console.log('attendees: ' + $scope.attendees);
+      $scope.attendees.forEach(function(guest){
+        ezfb.api('https://graph.facebook.com/'+ guest.id + '?fields=picture,name,hometown', function(res){
+        console.log(res);
+        guest.picture = res.picture.data.url;
+        guest.hometown = res.hometown;
+        console.log(guest);
+      });
+      });
+    };
+  })
   .controller('MyCtrl2', [function() {
 
   }]);
+
